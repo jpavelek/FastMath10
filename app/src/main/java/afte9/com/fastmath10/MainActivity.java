fd884777483566b952b2f3098445233f335cff90
Jakub Pavelek <jpavelek@live.com>
2017-06-28 21:42:13 +0200

    Small layout updates

--- app/src/main/java/afte9/com/fastmath10/MainActivity.java
+++ app/src/main/java/afte9/com/fastmath10/MainActivity.java
@@ -3,6 +3,8 @@ package afte9.com.fastmath10;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_screen, menu);
+        return true;
     }
 
     public void launchGame(View view) {
