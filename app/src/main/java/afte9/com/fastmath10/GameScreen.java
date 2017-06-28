8d1862b980b0486791e9901dc409db39cafd68b5
Jakub Pavelek <jpavelek@live.com>
2017-06-28 22:08:37 +0200

    Added timer and countdowns.

--- app/src/main/java/afte9/com/fastmath10/GameScreen.java
+++ app/src/main/java/afte9/com/fastmath10/GameScreen.java
@@ -2,19 +2,40 @@ package afte9.com.fastmath10;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class GameScreen extends AppCompatActivity {
 
+    private int score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
 
-        //Enable "BAck" arrow
+        //Enable "Back" arrow
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Reset game from start
+        score = 0;
+
+
+        //Start timer and game
+        new CountDownTimer(30000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar);
+                if (bar == null) return;
+                bar.setProgress((int) millisUntilFinished / 1000);
+            }
+            public void onFinish() {
+                //TODO - End game
+            }
+        }.start();
+
+
     }
 
     //If Back arrow clicked, go back
@@ -22,4 +43,6 @@ public class GameScreen extends AppCompatActivity {
         finish();
         return true;
     }
+
+
 }
--- app/src/main/java/afte9/com/fastmath10/MainActivity.java
+++ app/src/main/java/afte9/com/fastmath10/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main_screen, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_reset_scores:
+                //TODO - reset scores
+                System.out.println("Reset scores ... not implemented");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void launchGame(View view) {
